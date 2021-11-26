aa5ed06f9df2746b8d95709c1fae2393e3b401b6
ZSI <89996056+ZSIOffical@users.noreply.github.com>
2021-11-26 21:10:55 +0100

    Update command for loading.lua

--- Other Scripts/command for loading.lua
+++ Other Scripts/command for loading.lua
@@ -3,13 +3,7 @@ local playerHead = game.Players.LocalPlayer.Character.Head
 
 
 Player.Chatted:connect(function(cht)
-    if cht:match(":give") then
-        for i, v in pairs(game:GetService("Workspace").Ignored:GetDescendants()) do
-            if v.Name == "TouchInterest" and v.Parent then
-               firetouchinterest(playerHead, v.Parent, 0)
-               wait(0.1)
-               firetouchinterest(playerHead, v.Parent, 1)
-            end
-        end 
+    if cht:match("your command") then
+            "your script"
     end
 end)
